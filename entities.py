5744b94d5275f142c1ebfa0b744fb949e8ab6dec
Yusyra Hossain <yusyra@gmail.com>
2026-03-10 11:07:55 -0400

    DSfds

--- entities.py
+++ entities.py
@@ -1,4 +1,7 @@
 class Vertex:
+    species: str
+    neighbours: dict[str, int]
+    
     def __init__(self, species: str) -> None:
         self.species = species
         self.neighbours = {}  # neighbour_species -> weight
@@ -6,3 +9,7 @@ class Vertex:
 class Graph:
     def __init__(self) -> None:
         self._vertices = {}  # species -> Vertex
+    
+    def add_vertex(self, species: str) -> None:
+        if species not in self._vertices:
+            self._vertices[species] = Vertex(species)
