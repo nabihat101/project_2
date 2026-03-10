2fd9f67140d5be4de5d1fe8226cad04863b0bdbf
Yusyra Hossain <yusyra@gmail.com>
2026-03-10 12:35:38 -0400

    add_edge

--- entities.py
+++ entities.py
@@ -8,10 +8,28 @@ class Vertex:
 
 class Graph:
     _vertices: dict[str, Vertex]
-    
+
     def __init__(self) -> None:
         self._vertices = {}  # species -> Vertex
     
     def add_vertex(self, species: str) -> None:
         if species not in self._vertices:
             self._vertices[species] = Vertex(species)
+    
+    def add_edge(self, s1: str, s2: str) -> None:
+        self.add_vertex(s1)
+        self.add_vertex(s2)
+
+        v1 = self._vertices[s1]
+        v2 = self._vertices[s2]
+
+        # Adding weights to the edges
+        if s2 in v1.neighbours:
+            v1.neighbours[s2] += 1
+        else:
+            v1.neighbours[s2] = 1
+
+        if s1 in v2.neighbours:
+            v2.neighbours[s1] += 1
+        else:
+            v2.neighbours[s1] = 1
