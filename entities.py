3b64904c232ac0911d0da53100b329aaa36777ee
Yusyra Hossain <yusyra@gmail.com>
2026-03-14 18:11:15 -0400

    Cleaned up, added docstrings

--- entities.py
+++ entities.py
@@ -1,22 +1,28 @@
 class Vertex:
     species: str
-    neighbours_weight: dict[str, int]
+    neighbours: dict[str, int]
 
     def __init__(self, species: str) -> None:
         self.species = species
         self.neighbours = {}  # neighbour_species -> weight
+
+    def __repr__(self) -> str:
+        return f"Vertex(species={self.species}, neighbours={self.neighbours})"
+
 
 class Graph:
     _vertices: dict[str, Vertex]
 
     def __init__(self) -> None:
         self._vertices = {}  # species -> Vertex
-    
+
     def add_vertex(self, species: str) -> None:
         if species not in self._vertices:
             self._vertices[species] = Vertex(species)
-    
+
     def add_edge(self, s1: str, s2: str) -> None:
+        if s1 == s2:
+            return
         self.add_vertex(s1)
         self.add_vertex(s2)
 
@@ -33,3 +39,13 @@ class Graph:
             v2.neighbours[s1] += 1
         else:
             v2.neighbours[s1] = 1
+
+class Vertex: 
+    species: str
+    neighbours_weight: dict[str, int]
+
+    def __init__(self, species: str) -> None:
+        self.species = species
+        self.neighbours = {}  # neighbour_species -> weight
+    
+    
