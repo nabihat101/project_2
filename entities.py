1a245a9c26b1d4d8afc5df7af60cd17729ab86f9
nabihat101 <nabiha.t101@gmail.com>
2026-03-16 13:32:30 -0400

    Update entities.py

--- entities.py
+++ entities.py
@@ -1,9 +1,23 @@
-"""
+""" CSC111 Project 2
+
+Module Description
+==================
 This file contains all the classes used to represent the data and the graph structure for our analysis.
 The main classes are:
 - Observation: represents a species and the locations it was observed in different seasons
 - Vertex: represents a vertex in the graph, which corresponds to a species in this case
 - Graph: represents the graph structure 
+
+Copyright and Usage Information
+===============================
+
+This file is provided solely for the personal and private use of students
+taking CSC111 at the University of Toronto St. George campus. All forms of
+distribution of this code, whether as given or with any changes, are
+expressly prohibited. For more information on copyright for CSC111 materials,
+please consult our Course Syllabus.
+
+This file is Copyright (c) 2026 by Nabiha Tariq, Yusyra Hossain, Eleanor Neal, Ruoshui Deng
 """
 
 class Observation:
