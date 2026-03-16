8576351f4558dbee2e85916bddbab64e643617b2
nabihat101 <nabiha.t101@gmail.com>
2026-03-16 13:34:00 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,3 +1,21 @@
+""" CSC111 Project 2
+
+Module Description
+==================
+This file contains all the methods used to calculate probability of two species interacting and to calculate distance between two locations (longitude and latitude) 
+
+Copyright and Usage Information
+===============================
+
+This file is provided solely for the personal and private use of students
+taking CSC111 at the University of Toronto St. George campus. All forms of
+distribution of this code, whether as given or with any changes, are
+expressly prohibited. For more information on copyright for CSC111 materials,
+please consult our Course Syllabus.
+
+This file is Copyright (c) 2026 by Nabiha Tariq, Yusyra Hossain, Eleanor Neal, Ruoshui Deng
+"""
+
 
 def calculate_interaction_likelihood(co_occurrences: int, obs_a: int, obs_b: int) -> float:
     """
