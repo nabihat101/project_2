0e4f1a6b9932b660c1ae61291fc62f09eb50fc80
Yusyra Hossain <yusyra@gmail.com>
2026-03-17 14:08:32 -0400

    added condition for proximity before edges

--- entities.py
+++ entities.py
@@ -20,6 +20,8 @@ please consult our Course Syllabus.
 This file is Copyright (c) 2026 by Nabiha Tariq, Yusyra Hossain, Eleanor Neal, Ruoshui Deng
 """
 
+from utils import harversine_distance_km
+
 class Observation:
     """
     Takes each observation and stores it in an object to make it easier to manipulate and build the graph.
@@ -132,7 +134,8 @@ class Graph:
             source_species = species_seen_in_season[i]
             for j in range(i + 1, len(species_seen_in_season)):
                 target_species = species_seen_in_season[j]
-                if source_species != target_species:
+                if source_species != target_species and harversine_distance_km(target_species, source_species) < 0.5:  
+                    # Assuming a proximity threshold of 0.5 km for co-occurrence
                     self.add_edge(source_species, target_species)
 
     def __repr__(self) -> str:
