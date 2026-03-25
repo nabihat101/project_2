3aa2a0911fadb97c46d7ba8169832ea2260b4bc2
nabihat101 <nabiha.t101@gmail.com>
2026-03-25 08:46:13 -0400

    Update run.py

--- run.py
+++ run.py
@@ -1,3 +1,21 @@
+""" CSC111 Project 2
+
+Module Description
+==================
+This file contains the methods to run and create the graph visualization along with user input
+
+Copyright and Usage Information
+===============================
+
+This file is provided solely for the personal and private use of students
+taking CSC111 at the University of Toronto St. George campus. All forms of
+distribution of this code, whether as given or with any changes, are
+expressly prohibited. For more information on copyright for CSC111 materials,
+please consult our Course Syllabus.
+
+This file is Copyright (c) 2026 by Nabiha Tariq, Yusyra Hossain, Eleanor Neal, Ruoshui Deng
+"""
+
 import data_manipulation, entities
 from utils import make_circular
 import os
