969f1322942af493aef89b77bb9e943a02ec3810
nabihat101 <nabiha.t101@gmail.com>
2026-03-25 08:45:23 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@
 
 Module Description
 ==================
-This file contains all the methods used to calculate probability of two species interacting and to calculate distance between two locations (longitude and latitude) 
+This file contains all the methods used to calculate probability of two species interacting and to calculate distance between two locations (longitude and latitude)
 
 Copyright and Usage Information
 ===============================
@@ -15,19 +15,19 @@ please consult our Course Syllabus.
 
 This file is Copyright (c) 2026 by Nabiha Tariq, Yusyra Hossain, Eleanor Neal, Ruoshui Deng
 """
-
+import numpy as np
 
 def calculate_interaction_likelihood(co_occurrences: int, obs_a: int, obs_b: int) -> float:
     """
     Calculates the likelihood of interaction between two species using the Jaccard Index. Returns a float between 0.0 (never interact) and 1.0 (always interact).
 
     We say that two species are more likely to interact when they are closer to each other.
-    
+
     Special notes on implementation:
         co_occurrences: The number of times the two species were observed
                         within your proximity/time thresholds.
         obs_a: Total number of observations for Species A in that season.
-        obs_b: Total number of observations for Species B in that season.  
+        obs_b: Total number of observations for Species B in that season.
     """
     if obs_a == 0 and obs_b == 0:
         return 0.0
@@ -51,17 +51,17 @@ def haversine_distance_km(loc1: tuple[float, float], loc2: tuple[float, float]) 
     lat1, lon1 = loc1
     lat2, lon2 = loc2
 
-    # account for earth's curvature 
+    # account for earth's curvature
     radius_km = 6371.0
 
-    # convert into radians 
+    # convert into radians
     phi1 = math.radians(lat1)
     phi2 = math.radians(lat2)
 
     # difference in latitudes
     d_phi = math.radians(lat2 - lat1)
 
-    # difference in longitudes 
+    # difference in longitudes
     d_lambda = math.radians(lon2 - lon1)
 
     # apply haversine formula
@@ -70,9 +70,48 @@ def haversine_distance_km(loc1: tuple[float, float], loc2: tuple[float, float]) 
 
     return radius_km * c
 
+def make_circular(img: np.ndarray) -> np.ndarray:
+    """
+    Return a circular image
+    Assume img is a np.ndarray which is an array that holds all the pixels
+    """
+
+    # img.shape returns (height, width, channels) but we only need height and width so we split
+    h, w = img.shape[:2]
+
+    # find center which is half the width and height and is a corrdinate
+    center = (int(w / 2), int(h / 2))
+
+    # find the radius which is the minimum of center tuple
+    radius = min(center[0], center[1])
+
+    # create a corrdinate grid using matplotlib
+    Y, X = np.ogrid[:h, :w]
+
+    # find distance from center using distance formula
+    dist_from_center = (X - center[0]) ** 2 + (Y - center[1]) ** 2
+
+    # true/false variable to ensure that distance is less than diameter
+    mask = dist_from_center <= radius ** 2
+
+    # ensure that img.shape[2] has rgb colours
+    if img.shape[2] == 3:
+
+        # creates a grid of 255's. 255 means it's visible and 0 means it's invisible (the pixel)
+        alpha = np.ones((h, w), dtype=np.uint8) * 255
+
+        # stack the alpha onto the image to become rgba
+        img = np.dstack((img, alpha))
+
+    # apply mask (outside circle = transparent) and the 3 is the alpha stack we added previously
+
+    img[~mask, 3] = 0
+
+    return img
+
 # import python_ta
-   # python_ta.check_all(config={
-    #'extra-imports': ['pandas', 'networkx'],  # the names (strs) of imported modules
-    #'allowed-io': [],     # the names (strs) of functions that call print/open/input
-    #'max-line-length': 120
-#})
+# python_ta.check_all(config={
+# 'extra-imports': ['pandas', 'networkx'],  # the names (strs) of imported modules
+# 'allowed-io': [],     # the names (strs) of functions that call print/open/input
+# 'max-line-length': 120
+# })
